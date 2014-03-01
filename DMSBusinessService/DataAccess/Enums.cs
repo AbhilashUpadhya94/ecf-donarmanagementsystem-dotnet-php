90cf7e9ab5902d9562b54ece211dd20d97f399d9
vijayamadhavareddy <vijayamadhavareddy@yahoo.com>
2014-03-01 16:07:55 +0530

    Pushing

---
+++ DMSBusinessService/DataAccess/Enums.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Enums
+{
+    public enum Status
+    {
+        Active,
+        Prosoective,
+        Dormant,
+        None
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Transgender,
+        None
+    }
+
+    public enum Segmentation
+    {
+        Silver,
+        Gold,
+        Diamond,
+        Platinum
+    }
+
+    public enum Bucket
+    {
+        Individual,
+        TrustOrFoundation,
+        Group
+    }
+
+    public enum Interval
+    {
+        OneTime,
+        Monthly,
+        HalfYearly,
+        Quarterly,
+        Annually,
+        None
+    }
+
+    public enum LoginStatus
+    {
+        LoggedIn,
+        Expired,
+        Error,
+        Blocked
+    }
+}
